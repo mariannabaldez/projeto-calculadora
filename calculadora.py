a2effcd110aab30c3e5605f384b186e395e167a0
caiomorozini <87106966+caiomorozini@users.noreply.github.com>
2021-12-11 00:33:59 -0300

    updated with class

--- calculadora.py
+++ calculadora.py
@@ -1,39 +1,66 @@
-def desenho_calculadora():
-    desenho_calculadora = [[7, 8, 9],
-                           [4, 5, 6],
-                           [1, 2, 3],
-                           [0, "", ""]]
-    print(f"{desenho_calculadora}")
+class Calculadora:
+    def __init__(self):
+        self.numero = int(input("primeiro valor:"))
+        self.numero2 = int(input("segundo valor: "))
 
 
-def soma(numero, numero2):
-    return numero + numero2
+    @classmethod
+    def operacao(cls):
+        cls.soma
+
+    @property
+    def soma(self):
+        return self.numero + self.numero2
+
+    @property
+    def subtrai(self):
+        return self.numero - self.numero2
+
+    @property
+    def multiplica(self):
+        return self.numero * self.numero2
+
+    @property
+    def divide(self):
+        return self.numero / self.numero2
+
+    @property
+    def calcula_resto(self):
+        return self.numero % self.numero2
 
 
-def subtrai(numero, numero2):
-    return numero - numero2
+#x = Calculadora()
+
+#resultado_soma = x.soma
+#print(resultado_soma)
+valor1 = input("Digite o primeiro valor: ")
+operacao = input("Operação: ")
+valor2 = input("segundo valor: ")
+
+import operator
+
+operacoes = {"+": operator.add,
+             "-": operator.sub,
+             "*": operator.mul,
+             "/": operator.mod,
+            }
+operacoes.get()
+
+print(valor1 + operacao + valor2)
+# numero = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+# operacao = input("Digite a operação que deseja fazer na conta: "
+#
+# "(somar, subtrair, multiplicar, etc)")
+#
+#
+# contas(numero,numero2,operacao)
+#
+# if operacao == "somar": soma(numero, numero2)
+# elif operacao == "subtrair": subtrai(numero, numero2)
+# elif operacao == "multiplicar": multiplica(numero, numero2)
+# elif operacao == "dividir": dividi(numero, numero2)
+# elif operacao == "resto": resto(numero, numero2)
 
 
-def multiplica(numero, numero2):
-    return numero * numero2
 
-
-def divide(numero, numero2):
-    return numero / numero2
-
-
-def calcula_resto(numero, numero2):
-    return numero % numero2
-
-
-numero = int(input("Digite o primeiro número: "))
-numero2 = int(input("Digite o segundo número: "))
-operacao = input("Digite a operação que deseja fazer na conta: "
-                 "(somar, subtrair, multiplicar, etc)")
-
-
-if operacao == "somar": soma(numero, numero2)
-elif operacao == "subtrair": subtrai(numero, numero2)
-elif operacao == "multiplicar": multiplica(numero, numero2)
-elif operacao == "dividir": dividi(numero, numero2)
-elif operacao == "resto": resto(numero, numero2)
